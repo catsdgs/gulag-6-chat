3a14902af8e8835357cfabf4ac7756190450ec74
D.J. Saderholm <catsdgs@disr.it>
2020-10-17 20:14:17 -0600

    Something was edited.
    I must have forgotten to tell you what I changed. Sorry.

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ io.on('connection', (socket) => {
     var userList = (users.getUserList(params.room);
     if (!isRealString(params.name) || !isRealString(params.room)) {
       return callback('Name and room name are required');
-    } else if (userList.includes(params.name)) {
+    } else if (userList.includes(params.name) === true) {
       return callback('Someone in this room already has this name. Please change it');
     } else {
     socket.join(params.room);
